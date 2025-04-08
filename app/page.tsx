fe038dc7d9d2e259342f2b7741bbfce2a7a050a8
Tim Benniks <tbenniks@gmail.com>
2025-04-08 13:27:56 +0200

    chore: initialize project with package.json, postcss, and tsconfig
    - Added package.json with dependencies and scripts for Next.js and GraphQL Codegen.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added tsconfig.json for TypeScript configuration with strict settings.

---
+++ app/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import Image from "next/image";
+import ContentstackLivePreview from "@contentstack/live-preview-utils";
+import { getPage, initLivePreview } from "@/lib/contentstack";
+import { useEffect, useState } from "react";
+import { Page } from "@/lib/types";
+
+export default function Home() {
+  const [page, setPage] = useState<Page>();
+
+  const getContent = async () => {
+    const page = await getPage("/");
+    setPage(page as Page);
+  };
+
+  useEffect(() => {
+    initLivePreview();
+    ContentstackLivePreview.onEntryChange(getContent);
+  }, []);
+
+  return (
+    <main className="max-w-(--breakpoint-md) mx-auto">
+      <section className="p-4">
+        {page?.title ? (
+          <h1
+            className="text-4xl font-bold mb-4"
+            {...(page?.$ && page?.$.title)}
+          >
+            {page?.title}
+          </h1>
+        ) : null}
+
+        {page?.description ? (
+          <p className="mb-4" {...(page?.$ && page?.$.description)}>
+            {page?.description}
+          </p>
+        ) : null}
+
+        {page?.image ? (
+          <Image
+            className="mb-4"
+            width={768}
+            height={414}
+            src={page?.image.url}
+            alt={page?.image.title}
+            {...(page?.image.$ && page?.image.$.url)}
+          />
+        ) : null}
+
+        {page?.rich_text ? (
+          <div
+            {...(page?.$ && page?.$.rich_text)}
+            dangerouslySetInnerHTML={{ __html: page?.rich_text }}
+          />
+        ) : null}
+
+        <div
+          className="space-y-8 max-w-full mt-4"
+          {...(page?.$ && page?.$.blocks)}
+        >
+          {page?.blocks?.map((item, index) => {
+            const { block } = item;
+            const isImageLeft = block.layout === "image_left";
+
+            return (
+              <div
+                key={`${block}-${index}`}
+                {...(page?.$ && page?.$[`blocks__${index}`])} // Adding editable tags if available
+                className={`flex flex-col md:flex-row items-center space-y-4 md:space-y-0 bg-white ${
+                  isImageLeft ? "md:flex-row" : "md:flex-row-reverse" // Adjusting the layout based on the block's layout property
+                }`}
+              >
+                <div className="w-full md:w-1/2">
+                  {block.image ? (
+                    <Image
+                      src={block.image.url}
+                      alt={block.image.title}
+                      width={200}
+                      height={112}
+                      className="w-full"
+                      {...(block?.image.$ && block?.image.$.url)}
+                    />
+                  ) : null}
+                </div>
+                <div className="w-full md:w-1/2 p-4">
+                  {block.title ? (
+                    <h2
+                      className="text-2xl font-bold"
+                      {...(block?.$ && block?.$.title)}
+                    >
+                      {block.title}
+                    </h2>
+                  ) : null}
+                  {block.copy ? (
+                    <div
+                      {...(block?.$ && block?.$.copy)}
+                      dangerouslySetInnerHTML={{ __html: block.copy }}
+                      className="prose"
+                    />
+                  ) : null}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ lib/contentstack.ts
@@ -0,0 +1,141 @@
+import contentstack from "@contentstack/delivery-sdk"
+import ContentstackLivePreview, { IStackSdk } from "@contentstack/live-preview-utils";
+import { GraphQLHeaders, Page } from "./types";
+import { GraphQLClient } from "graphql-request";
+import { graphql } from "../gql"
+import { getContentstackEndpoints, getRegionForString } from "@timbenniks/contentstack-endpoints";
+
+const region = getRegionForString(process.env.NEXT_PUBLIC_CONTENTSTACK_REGION as string)
+const endpoints = getContentstackEndpoints(region, true)
+
+export const stack = contentstack.stack({
+  apiKey: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+  deliveryToken: process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN as string,
+  environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT as string,
+  region: region,
+  live_preview: {
+    enable: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true',
+    preview_token: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW_TOKEN,
+    host: endpoints.preview,
+  }
+});
+
+export function initLivePreview() {
+  ContentstackLivePreview.init({
+    ssr: false,
+    enable: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true',
+    mode: "builder",
+    stackSdk: stack.config as IStackSdk,
+    stackDetails: {
+      apiKey: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+      environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT as string,
+    },
+    clientUrlParams: {
+      host: endpoints.application
+    },
+    editButton: {
+      enable: true,
+      exclude: ["outsideLivePreviewPortal"]
+    },
+  });
+}
+
+export async function getPage(url: string) {
+  const apiKey = process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY;
+  const environment = process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT;
+  const accessToken = process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN as string;
+  const preview = process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW;
+  const previewToken = process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW_TOKEN as string;
+  const hash = ContentstackLivePreview.hash;
+  const baseURL = preview === 'true' && hash ? endpoints.graphqlPreview : endpoints.graphql
+
+  const headers: GraphQLHeaders = {
+    access_token: accessToken
+  }
+
+  if (hash) {
+    headers.live_preview = hash;
+    headers.preview_token = previewToken
+  }
+
+  const gqEndpoint = `https://${baseURL}/stacks/${apiKey}?environment=${environment}`;
+  const graphQLClient = new GraphQLClient(gqEndpoint, {
+    headers
+  })
+
+  const query = graphql(`
+    query Page($url: String!) {
+      all_page(where: {url: $url}) {
+        items {
+          system {
+            uid
+            content_type_uid
+          }
+          description
+          rich_text
+          title
+          url
+          imageConnection {
+            edges {
+              node {
+                url
+                title
+              }
+            }
+          }
+          blocks {
+            ... on PageBlocksBlock {
+              __typename
+              block {
+                copy
+                imageConnection {
+                  edges {
+                    node {
+                      url
+                      title
+                    }
+                  }
+                }
+                layout
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const variables = {
+    url: url || "/",
+  };
+
+  const res = await graphQLClient.request(
+    query,
+    variables
+  );
+
+  // needed for editable tags
+  const fullPage = res?.all_page?.items && res?.all_page?.items[0]
+  const fixedEntryForEditableTags = res?.all_page?.items && {
+    ...fullPage,
+    image: fullPage?.imageConnection?.edges && fullPage?.imageConnection?.edges[0]?.node,
+    blocks: fullPage?.blocks && fullPage?.blocks.map(block => ({
+      ...block,
+      block: {
+        ...block?.block,
+        image: block?.block && block?.block.imageConnection?.edges?.[0]?.node || null
+      }
+    })),
+    uid: res?.all_page?.items[0]?.system?.uid,
+    _content_type_uid: res?.all_page?.items[0]?.system?.content_type_uid
+  }
+
+  const entry = fixedEntryForEditableTags;
+
+  if (process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true') {
+    entry && contentstack.Utils.addEditableTags(entry as Page, 'page', true);
+  }
+
+  return entry as Page
+}
